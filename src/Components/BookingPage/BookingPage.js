094d85a720f5a5f4c67beccefa44704158da83aa
khangdtg <63592048+dtgkhang@users.noreply.github.com>
2023-03-19 22:52:25 +0700

    fix booking detail

--- src/Components/BookingPage/BookingPage.js
+++ src/Components/BookingPage/BookingPage.js
@@ -115,6 +115,29 @@ function BookingPage(){
 <div>
 
 
+      <section className="breadcrumb-area bread-bg-6 py-0">
+        <div className="breadcrumb-wrap">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-12">
+                <div className="breadcrumb-btn">
+                  <div className="btn-box">
+                    <a className="theme-btn" data-fancybox="video" data-src="https://www.youtube.com/watch?v=IpVmKLpo-Sk" data-speed={700}>
+                      <i className="la la-video-camera mr-2" />Video
+                    </a>
+                    <a className="theme-btn" data-src="images/destination-img.jpg" data-fancybox="gallery" data-caption="Showing image - 01" data-speed={700}>
+                      <i className="la la-photo mr-2" />More Photos
+                    </a>
+                  </div>
+                  <a className="d-none" data-fancybox="gallery" data-src="images/destination-img2.jpg" data-caption="Showing image - 02" data-speed={700} />
+                  <a className="d-none" data-fancybox="gallery" data-src="images/destination-img3.jpg" data-caption="Showing image - 03" data-speed={700} />
+                  <a className="d-none" data-fancybox="gallery" data-src="images/destination-img4.jpg" data-caption="Showing image - 04" data-speed={700} />
+                </div>{/* end breadcrumb-btn */}
+              </div>{/* end col-lg-12 */}
+            </div>{/* end row */}
+          </div>{/* end container */}
+        </div>{/* end breadcrumb-wrap */}
+      </section>
       <section className="tour-detail-area padding-bottom-90px">
         <div className="single-content-navbar-wrap menu section-bg" id="single-content-navbar">
           <div className="container">
